3d1af38deaaa433ec21a3db1c6e3516865a703a3
PineApples11 <kinyanjuiivy1@gmail.com>
2025-03-05 12:46:53 +0300

    correction

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,44 +1,47 @@
 class Pet:
-    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
-    all=[]
 
-    def __init__(self,name,pet_type,optional_owner=None):
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    all = []
 
-        if pet_type not in self.PET_TYPES:
-            raise Exception("Pet type is invalid")
-
-        self.name=name
-        self.pet_type=pet_type
-        self.owner=optional_owner
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
         Pet.all.append(self)
-        pass
-    pass
+
+    @property
+    def pet_type(self):
+        return self._pet_type
+
+    @pet_type.setter
+    def pet_type(self, pet_type):
+        if pet_type not in self.PET_TYPES:
+            raise Exception('Not a valid pet type.')
+        self._pet_type = pet_type
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, owner):
+        if not (isinstance(owner, Owner) or not owner):
+            raise Exception("Object is not of type Owner")
+        self._owner = owner
 
 
-    # @property
-    # def pet_type(self):
-    #     self._pet_type
 
-    # @pet_type.setter
-    # def pet_type(self,pet):
-        
-      
-        
 class Owner:
-    def __init__(self,name):
-        self.name=name
+    def __init__(self, name):
+        self.name = name
 
     def pets(self):
         return [pet for pet in Pet.all if pet.owner == self]
-    
-    def add_pet(self,pet):
-        if isinstance(pet,Pet):
-            pet.owner=self
-        else:
-            raise Exception ("Couldnt add owner")
-        
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Input object is not of type Pet")
+        pet.owner = self            
+
     def get_sorted_pets(self):
         return sorted(self.pets(), key=lambda pet: pet.name)
-
-        
-    pass
